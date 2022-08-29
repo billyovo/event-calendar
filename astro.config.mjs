cd32573a004bcb91ab2abc66ad0b8da230c742dd
billyovo <30436689+billyovo@users.noreply.github.com>
2022-08-29 20:27:43 +0800

    try fix image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://billyovo.github.io',
-  base: '/event-calendar',
-})
+  base: '/event-calendar'
+});
