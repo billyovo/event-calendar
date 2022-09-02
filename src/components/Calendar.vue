cbf4c1de4517c965f1290d2924fef5985b0558e2
billyovo <30436689+billyovo@users.noreply.github.com>
2022-09-02 19:09:32 +0800

    add record

---
+++ src/components/Calendar.vue
@@ -0,0 +1,144 @@
+<script>
+    import event from '../assets/event.json';
+    import { RRule } from 'rrule'
+    
+    export default {
+
+      data(){
+        return{
+            emptyPaddingCell: 0,
+            dateCells: 0,
+            RRule: RRule,
+
+            events: event,
+            occurence: {},
+
+            weekdays: ["日", "一", "二", "三","四","五","六"],
+            today: new Date(),
+            selectedMonth: new Date(),
+        }
+      },
+      methods:{
+        fillEmptyCells(){
+            
+            const firstDay = new Date(this.selectedMonth.getFullYear(), this.selectedMonth.getMonth(), 1);
+            
+            this.emptyPaddingCell = firstDay.getDay();
+        },
+        fillDateCells(){
+          const lastDay = new Date(this.selectedMonth.getFullYear(), this.selectedMonth.getMonth(),0);
+          this.dateCells = lastDay.getDate();
+          
+        },
+        changeMonth(month){
+          this.selectedMonth = new Date(this.selectedMonth.getFullYear(), this.selectedMonth.getMonth()+month, 1);
+          this.getOccurence();
+          this.fillEmptyCells();
+          this.fillDateCells();
+        },
+        getOccurence(){
+          this.occurence = {};
+          for(let i = 0;i<this.events.length;i++){
+            const rule = RRule.fromString(this.events[i].rrule);
+            const occurence = rule.between(this.selectedMonth, new Date(this.selectedMonth.getFullYear(), this.selectedMonth.getMonth()+1, 0));
+            for(let j = 0;j<occurence.length;j++){
+              const date = occurence[j];
+              this.occurence[date.getDate()] = {
+                title: this.events[i].title,
+                imageurl: this.events[i].imageurl
+              }
+            }
+          }
+          console.log(this.occurence);
+        }
+      },
+      mounted(){ 
+        this.fillEmptyCells();
+        this.fillDateCells();
+        this.getOccurence();
+      },
+    }
+</script>
+
+<template>
+  <div>
+    <div class="tool-bar">
+      <button @click="changeMonth(-1)" :style="(today-selectedMonth) < 0 ? null : 'visibility: hidden'" class="control-button"><i class="fa-solid fa-arrow-left"></i> LEft</button>
+      <h2 class="selected">{{selectedMonth.getFullYear()}} / {{selectedMonth.getMonth()+1}}</h2>
+      <button @click="changeMonth(1)" :style="(selectedMonth-today) < 31556952000 ? null : 'visibility: hidden'" class="control-button">right <i class="fa-solid fa-arrow-right"></i></button>
+      </div>
+    
+      <div class="container">
+          <div v-for="weekday in weekdays" :key="weekday" class="weekday wrapper">
+              <h3 class="title">{{weekday}}</h3>
+          </div>
+          <div class="wrapper cell empty" v-for="index in emptyPaddingCell" :key="index"></div>
+          <div class="wrapper cell" v-for="index in dateCells" :key="index">
+            <div>
+              <span :style="(today.getFullYear() === selectedMonth.getFullYear() && today.getMonth() === selectedMonth.getMonth()) && (today.getDate() === index)? 'color: red' : null">{{index}}</span>
+              <div v-if="occurence[index]" class="wrapper-content">
+                <span class="wrapper-content-title">{{occurence[index].title}}</span>
+                <img :src="occurence[index].imageurl" height="50">
+              </div>
+            </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+    .container{
+        padding: 1px;
+        display: grid;
+        grid-template-columns: repeat(7, 1fr);
+        background-color: gray;
+        grid-gap: 1px;
+    }
+    .tool-bar{
+      display: flex;
+      justify-content: space-between;
+      align-content: center;
+      width: 100%;
+      margin-bottom: 20px;
+    }
+    .control-button{
+      background: none;
+      border: none;
+      color: var(--shade-darken3);
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      font-size: 1.1rem;
+      cursor: pointer;
+    }
+
+    .weekday{
+        text-align: center;
+    }
+    .wrapper{
+      box-sizing: border-box;
+      background-color: white;
+    }
+    .wrapper-content{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      padding: 10px;
+    }
+    .wrapper-content-title{
+      margin-bottom: 5px;
+    }
+    .cell{
+      min-height: 50px;
+      min-width: 50px;
+    }
+    .empty{
+      background-color: gray;
+    }
+    .title{
+        font-size: 0.9rem;
+        margin: 0px;
+        padding: 10px;
+    }
+</style>
