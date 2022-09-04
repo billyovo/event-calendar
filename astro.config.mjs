b066a0ba533d0cdb9e93691835d47561b6665418
billyovo <30436689+billyovo@users.noreply.github.com>
2022-09-04 15:58:31 +0800

    fin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import vue from "@astrojs/vue";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), sitemap()],
   output: 'server',
   adapter: node()
 });
