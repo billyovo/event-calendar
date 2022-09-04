10d63d0a507871700ee442fcbe65161357e4ace8
billyovo <30436689+billyovo@users.noreply.github.com>
2022-09-04 16:05:15 +0800

    fix metatags

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), sitemap()],
+  site: "https://minigame.letsdream.today/",
   output: 'server',
   adapter: node()
 });
