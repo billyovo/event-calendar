8c6d08667b01ba83fa637b64cac5d7193821236c
billyovo <30436689+billyovo@users.noreply.github.com>
2022-09-04 23:03:54 +0800

    add tests

--- tests/page.test.js
+++ tests/page.test.js
@@ -1,10 +1,10 @@
-import { test, expect, errors } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
 const url = "https://minigame.letsdream.today";
 
 test('Landing page is working', async ({ page }) => {
     await page.goto(url);
-    await page.waitForLoadState('domcontentloaded')
+    await page.waitForLoadState('domcontentloaded');
     await expect(page).toHaveScreenshot();
 })
 
@@ -15,16 +15,31 @@ test('News can load', async ({ page }) => {
     expect(await page.locator(".line").count()).toBeGreaterThan(0);
 })
 
-test("Calendar Working", async({page})=>{
-    await page.goto(`${url}/calendar`);
-    const nextPageButton = await page.locator('.control-button');
 
+test("Calendar Working and value is correct", async({page})=>{
+    await page.goto(`${url}/calendar`);
+    let nextPageButton = await page.locator('button:has-text("下一頁")');
+    
     let startingDate = new Date();
-    while(nextPageButton.isVisible()){
+
+    while(await nextPageButton.isVisible()){
         let lastDate = new Date(startingDate.getFullYear(), startingDate.getMonth()+1, 0);
         await page.waitForSelector(".cell");
+
+        //calendar has correct cells
         expect(await page.locator(".cell:not(.empty)").count()).toEqual(lastDate.getDate());
-        startingDate = new Date(startingDate.getFullYear(), startingDate.getMonth()+1, 1);
-        nextPageButton.click();
-    }   
+        startingDate = new Date(startingDate.getFullYear(), startingDate.getMonth()+1, 1); 
+        
+        //and events are shown
+        expect(await page.locator(".cell img").count()).toBeGreaterThan(0);
+        await nextPageButton.click();
+    }
+    
+})
+
+test('Records are shown', async ({ page }) => {
+    await page.goto(`${url}/record`);
+    await page.waitForSelector(".card");
+
+    expect(await page.locator(".card").count()).toBeGreaterThan(0);
 })
