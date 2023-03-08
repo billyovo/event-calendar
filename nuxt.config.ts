452a04d57396c70432ece7c98c8fa18a8b786cd8
billyovo <30436689+billyovo@users.noreply.github.com>
2023-03-08 15:05:45 +0800

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    experimental:{
+        payloadExtraction: false
+    },
     app:{
         head:{
             title: "DC 築夢物語 - 綜合伺服器 | 小遊戲",
