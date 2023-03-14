acc0cab2fdd2ce1d92ff0cac3a0857a5cb9375e1
deadfish12 <61008844+deadfish12@users.noreply.github.com>
2023-03-13 20:45:13 -0700

    add intro, will add more soonTM

--- pages/introduction.vue
+++ pages/introduction.vue
@@ -8,7 +8,11 @@
         <NuxtLayout name="intro">
             <template #event>
                 <img src="https://via.placeholder.com/1000x1000" class="event-img" alt="blah blah blah">
-                <p>活動會在blah blah blah舉行, go by blah blah blah, how to get reward blah blah blah etc</p>
+                <p>築夢物語的小遊戲目前恆常舉行
+玩家可以在伺服器的重生點點擊 小遊戲魔法使NPC 前往小遊戲大廳
+往右走就可以根據指示前往小遊戲伺服器了
+
+製作人員: billyovo, deadfish12, KnowledgeBerry</p>
             </template>
         </NuxtLayout>
     </div>
