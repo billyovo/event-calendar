19e9c90e713f535903261ea065603f60770607b1
billyovo <30436689+billyovo@users.noreply.github.com>
2023-03-22 12:53:39 +0800

    remove to gh pages

--- components/FilterList.vue
+++ components/FilterList.vue
@@ -50,7 +50,7 @@
         <h3>玩家名稱</h3>
         <input type="search" v-model="name" placeholder="輸入名稱..." class="text-input">
         <span style="margin-top: 30px">頭像提供: <a style="color: inherit" href="https://crafatar.com">Crafatar</a></span>
-        <span style="margin-top: 20px"><a style="color: inherit" href="/docs">點我查看小遊戲紀錄API規格</a></span>
+        <span style="margin-top: 20px"><a style="color: inherit" href="https://billyovo.github.io/minigame-api/">點我查看小遊戲紀錄API規格</a></span>
     </aside>
 </template>
 <script>
