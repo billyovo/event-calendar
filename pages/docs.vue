2c43f94ad224a4b009ada3a0a9f5fb82c6a13fce
billyovo <30436689+billyovo@users.noreply.github.com>
2023-03-22 12:42:02 +0800

    copy and pasting code from stackoverflow

--- pages/docs.vue
+++ pages/docs.vue
@@ -12,6 +12,24 @@
                 content: content
             }
         },
+        mounted(){
+            function executeScriptElements(containerElement) {
+                const scriptElements = containerElement.querySelectorAll("script");
+
+                Array.from(scriptElements).forEach((scriptElement) => {
+                  const clonedElement = document.createElement("script");
+
+                  Array.from(scriptElement.attributes).forEach((attribute) => {
+                    clonedElement.setAttribute(attribute.name, attribute.value);
+                  });
+
+                  clonedElement.text = scriptElement.text;
+
+                  scriptElement.parentNode.replaceChild(clonedElement, scriptElement);
+                });
+            }
+            executeScriptElements(document.getElementsByTagName("body")[0]);
+        }
     }
 </script>
 
