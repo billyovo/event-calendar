320c5e335d430da6a187b15a99deb1ac21b3522a
deadfish12 <61008844+deadfish12@users.noreply.github.com>
2023-03-24 22:48:52 -0700

    Update introduction.vue

--- pages/introduction.vue
+++ pages/introduction.vue
@@ -7,7 +7,7 @@
         </Html>
         <NuxtLayout name="intro">
             <template #event>
-                <img src="https://via.placeholder.com/1000x1000" class="event-img" alt="blah blah blah">
+                <img src="~/assets/images/banner/lobby.png" class="event-img" alt="blah blah blah">
                 <p>築夢物語的小遊戲目前恆常舉行
 玩家可以在伺服器的重生點點擊 小遊戲魔法使NPC 前往小遊戲大廳
 往右走就可以根據指示前往小遊戲伺服器了
