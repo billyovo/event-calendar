260bd99e7e66f418cf1096823cfc0f0a7285158b
billyovo <30436689+billyovo@users.noreply.github.com>
2023-03-25 13:55:24 +0800

    why deadfish12 don't use list element for a list

--- pages/introduction.vue
+++ pages/introduction.vue
@@ -7,12 +7,12 @@
         </Html>
         <NuxtLayout name="intro">
             <template #event>
-                <img src="~/assets/images/banner/lobby.png" class="event-img" alt="blah blah blah">
+                <img src="~/assets/images/banner/lobby.png" class="event-img" alt="大廳照片">
                 <p>築夢物語的小遊戲目前恆常舉行
 玩家可以在伺服器的重生點點擊 小遊戲魔法使NPC 前往小遊戲大廳
-往右走就可以根據指示前往小遊戲伺服器了
+往右走就可以根據指示前往小遊戲伺服器了</p>
 
-小遊戲結束後所有參加了的玩家都可以拿到參加獎勵
+<p>小遊戲結束後所有參加了的玩家都可以拿到參加獎勵
 可以在小遊戲大廳的 小妹NPC 和 小遊戲大禮盒 使用幻夢之碎片
 獲勝的玩家在小遊戲結束的24小時後可以找小遊戲大廳的 小妹的朋友NPC 領取優勝獎勵
 結合幻夢之碎片可以與 小妹的新朋友NPC 兌換更厲害的優勝物品喔
